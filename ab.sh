cb5db48c591586caac13dcf43111b1e93a4533bc
Shantanu Bhargava <38581702+HACKER097@users.noreply.github.com>
2023-11-28 01:16:53 +0530

    Create ab.sh

---
+++ ab.sh
@@ -0,0 +1,5 @@
+q=$(echo "https://hdaudiobooks.com//?s=$@" | tr ' ' '+')
+html=$(curl -s "$q")
+name=$(echo "$html" | grep "<h2>" | cut -d'"' -f4 | sed -e 's/&#8211/\-/g;' | fzf | sed -e 's/\-/\&#8211/g')
+links=$(curl -s "$(echo "$html" | grep "$name" | cut -d'"' -f2 | head -n 1)" | grep "mp3" | cut -d'"' -f14)
+mpv $(echo "$links" | grep "=$(echo "$links" | cut -d "=" -f 2 | fzf)")
