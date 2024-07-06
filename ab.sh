df1d73ba257555efb1dec1aae25a2b0afaca76cd
Shantanu Bhargava <38581702+HACKER097@users.noreply.github.com>
2024-07-06 22:59:40 +0530

    Update ab.sh

--- ab.sh
+++ ab.sh
@@ -1,4 +1,4 @@
-q=$(echo "https://hdaudiobooks.com//?s=$@" | tr ' ' '+')
+q=$(echo "https://hdaudiobooks.net//?s=$@" | tr ' ' '+')
 html=$(curl -s "$q")
 name=$(echo "$html" | grep "<h2>" | cut -d'"' -f4 | sed -e 's/&#8211/\-/g;' | fzf | sed -e 's/\-/\&#8211/g')
 links=$(curl -s "$(echo "$html" | grep "$name" | cut -d'"' -f2 | head -n 1)" | grep "mp3" | cut -d'"' -f14)
